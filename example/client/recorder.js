7cbd52e01e53db8cbbf8aa534e19790d4b511862
Ian McGraw <mcgrawian@gmail.com>
2012-01-16 14:17:24 -0500

    fix IE9

--- example/client/recorder.js
+++ example/client/recorder.js
@@ -68,7 +68,7 @@ Wami.setup = function(id, callback) {
 			params.wmode = "transparent";
 		}
 
-		if (typeof console !== 'undefined') {
+		if (typeof window.console !== 'undefined') {
 			flashVars.console = true;
 		}
 
